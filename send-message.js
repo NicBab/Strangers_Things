cd3b421a90d7218d0f27eac8be57324e3aeb5827
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-05-18 17:58:04 -0500

    working on messagges

--- send-message.js
+++ send-message.js
@@ -1,14 +1,15 @@
 function sendMessage() {
   const url = `${BASE_URL}/posts/${postId}/messages`;
+  const userToken = localStorage.getItem("token")
     fetch(url, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer TOKEN_STRING_HERE'
+      'Authorization': `Bearer ${userToken}` 
     },
     body: JSON.stringify({
       message: {
-        content: "Do you still have this?  Would you take $10 less?"
+        content: "Do you still have this?  Would you take $10 less?"  // form .val()
       }
     })
   }).then(response => response.json())
@@ -17,10 +18,32 @@ function sendMessage() {
     })
     .catch(console.error);
 }
-    //Message send submit btn
-    $("#message-btn").on("click", async function (e) {
-        e.preventDefault();
-        let message = $("#message-modal").val();
-        console.log(message)
-        await message(message);
-      });
+
+    //Message send submit btn  
+      $('#items-post').on('click', "#message-button", async function (e) {
+    console.log("**message")
+  //click mssg makes modal visible
+    });
+
+
+    //make html modal
+    //click mssg makes modal visible
+    //modal form
+    //submit btn
+    //close btn
+
+
+
+
+    // const postElement = $(this).closest('.card');
+    // const post = postElement.data('post');
+    // try {
+    //   e.preventDefault();
+    //   let message = $("#message-modal").val();
+    //   console.log(message)
+    //   await message(message);
+
+    // } catch(error) {
+    //   console.error(error)
+    // }
+    
